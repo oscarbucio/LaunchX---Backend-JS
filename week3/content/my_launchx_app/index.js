487079ca31db676e01ad3b23f5f9fa64176ec081
Oscar Hernández Bucio <99281383+oscarbucio@users.noreply.github.com>
2022-05-13 14:38:49 -0500

    Delete week3/content directory

--- week3/content/my_launchx_app/index.js
+++
@@ -1,8 +0,0 @@
-/*
- * Recuerda: Para usar cualquier módulo necesitas haberlo exportado.
- * Necesitas indicar el path del archivo que vas a importar aquí de la forma que se indica (este varia dependiendo en qué archivo lo importas):
- * */
-const MissionCommander = require('./app/missionCommander');
-
-const carlo = new MissionCommander("Carlo")
-console.log(carlo.name)
